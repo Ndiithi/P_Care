0ae3a0a424883d11f63e49537089428a07a81fc9
ndiithi <duncanndiithi@yahoo.com>
2022-09-17 12:59:36 +0300

    fix typo

--- resources/js/components/reports/stocks/StocksReport.js
+++ resources/js/components/reports/stocks/StocksReport.js
@@ -57,14 +57,14 @@ class StocksReport extends React.Component {
                             data-toggle="tab" href="#tab2" role="tab"
                             aria-controls="1015"
                             aria-selected="false">
-                            Expiry btwn 10-15 days</a>
+                            Expiry next 10-15 days</a>
                     </li>
                     <li className="nav-item" role="presentation">
                         <a className="nav-link" id="1520-tab"
                             data-toggle="tab" href="#tab3" role="tab"
                             aria-controls="1520"
                             aria-selected="false">
-                            Expiry btwn 15-20 days</a>
+                            Expiry next 15-20 days</a>
                     </li>
 
                 </ul>
@@ -79,13 +79,13 @@ class StocksReport extends React.Component {
                     </div>
                     <div id="tab2"
                         className="tab-pane fade "  >
-                        <h6 className="text-left">Products expiring between 10-15 days</h6>
+                        <h6 className="text-left">Products expiring next 10-15 days</h6>
                         <br />
                         <ExpiryReport time={"10_15"} />
                     </div>
                     <div id="tab3"
                         className="tab-pane fade" > {/* if add org permission not defined, edit is defined as this pop up shows in either or both defined*/}
-                        <h6 className="text-left">Products expiring between 15-20 days</h6>
+                        <h6 className="text-left">Products expiring next 15-20 days</h6>
                         <br />
                         <ExpiryReport time={"15_20"} />
                     </div>
